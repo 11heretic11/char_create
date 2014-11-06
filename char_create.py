f0d78a64231e3fa06d066bf444e40fdd668ecdf1
Ivo Peres <11heretic11@gmail.com>
2014-11-06 22:29:12 +0200

    Initial Commit
    First commit for char_create
    
    Simple name input system
    Beginnings of a skill list

---
+++ char_create.py
@@ -0,0 +1,10 @@
+#Char_create
+#Ivo Peres 2014
+
+
+user_name = input('Enter your name: ')
+print(user_name)
+
+user_skill = ['WARRIOR' ,'ROGUE' , 'WIZZARD']
+list (enumerate(user_skill))
+print (list)
